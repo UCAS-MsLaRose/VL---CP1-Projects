d5830731ded5265f4e036ef0250622c648e0246a
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-08-19 09:53:59 -0600

    varibles notes

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Vienna laRose, Variables Notes
+
+# Create a variable
+name = "Katie"
+age = 14
+
+print("Hello " + name)
+
+# reset variable
+name = "Alex"
+
+print("Sorry I meant " + name)
+
+
+# another variable example
+print("Wow you are", age)
+print("Oh wait your birthday was yesterday?")
+age += 1
+print("So you are", age)
