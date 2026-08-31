ee77bb676210ca9880b9c791aeaa4e3b6e6190e1
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-08-31 09:53:40 -0600

    integers notes

---
+++ Notes/integers_floats.py
@@ -0,0 +1,22 @@
+# Integers and Floats notes, VL
+
+# Integer - A whole number 
+num = 75 # <= Just write the number no extra syntax 
+
+# Float # <= a number with a decimal point 
+pi = 3.1415 # <= Just write the number no extra syntax
+
+# Arithmetic Operators ( + - * / ** // %)
+print(5//2) # <= integer division (it only gives you the integer)
+print(4/2) # <= will always output a float
+
+# Modulo/mod = % (modulus)
+print(5%2) # <= Gives the remainder of a division problem
+print(10%4)
+print(15%5)
+
+print((2-1)*3+4%3)
+# Order of Operations (P E MMD AS)
+
+# assignment operator =
+
