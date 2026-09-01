b1ef33bec95c9ac85f73026d2806098f0f43b557
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-09-01 09:53:46 -0600

    int and float notes

--- Notes/integers_floats.py
+++ Notes/integers_floats.py
@@ -16,7 +16,23 @@ print(10%4)
 print(15%5)
 
 print((2-1)*3+4%3)
-# Order of Operations (P E MMD AS)
+# Order of Operations (P E MMD AS) left to right
 
 # assignment operator =
+print(f"Before {num}")
+#num = num + 2
+num += 2
+print(f"After {num}")
 
+num //= 3
+print(f"Other After {num}")
+
+num %= 4
+print(f"After Mod {num}")
+
+
+fav = input("What is your favorite number: ")
+
+print(f"{float(fav)**2} is {fav} squared!")
+print(round(num,6))
+print(int(pi))
