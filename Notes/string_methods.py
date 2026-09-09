1c6743baee82b4b237b6b5fe6d2d9721a4e6945a
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-09-09 13:19:47 -0600

    notes

--- Notes/string_methods.py
+++ Notes/string_methods.py
@@ -1,5 +1,5 @@
 # VL, String Methods
-
+"""
 sentence = "The quick brown fox jumps over the lazy dog"
 
 word = input("What word do you want?: ").strip().lower()
@@ -19,11 +19,22 @@ last_fixed = "".join(last_seperated)
 full_name = fixed.title() + " " + last_fixed.title()
 print("Hello " + full_name.title())
 
-print(full_name.isalpha())
-print(full_name.isnumeric())
-print(full_name.isupper())
+print(full_name.isalpha()) # Checks if the entire thing is characters
+print(full_name.isnumeric())# Checks if entire thing is numbers
+print(full_name.isupper())# Checks if all all the string is uppercase
 
 print(sentence.lower()) 
 print(sentence.upper())
 print(sentence.capitalize()) 
 print(sentence.title()) 
+
+
+# formatted string 
+print(f"Hello {fixed.title()} {last_fixed} welcome to my program!")"""
+
+letter = input("Give me a letter: ")
+letter = letter[0].lower()
+number_value = ord(letter)
+number_value += 2
+new_letter = chr(number_value)
+print(f"Your letter was {letter} now it is {new_letter}")
---
+++ Notes/using_random.py
@@ -0,0 +1,12 @@
+import random 
+
+ducks = random.randint(1,10)
+
+print(f"There are {ducks} ducks!")
+
+pens = random.randrange(1,15,3)
+print(f"I have {pens} pens.")
+
+percent = random.random()
+
+print(f"You have a {percent:.2} grade.")
